d2d0118a3215d897108987b9347fd78f62f336e5
Evil0genius <rybin.evgeney14@gmail.com>
2024-02-11 22:32:33 +0400

    Push data from database to webpage

--- src/main/java/com/example/demo/controllers/ArticleController.java
+++ src/main/java/com/example/demo/controllers/ArticleController.java
@@ -4,10 +4,13 @@ import com.example.demo.model.Article;
 import com.example.demo.repo.ArticleRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.Optional;
 
 
 @Controller
@@ -15,13 +18,22 @@ public class ArticleController {
     @Autowired
     private ArticleRepo articleRepo;
     @GetMapping("/")
-    public String mainPage(){
+    public String mainPage(Model model){
+        Iterable<Article> articles = articleRepo.findAll();
+        model.addAttribute("articles", articles);
         return "index";
     }
     @GetMapping("/bootstrap")
     public String addArticle() {
         return "addArticle";
     }
+    @GetMapping("/blog/{id}")
+    public String showArticle(@PathVariable(value = "id") long id, Model model){
+        Optional<Article> article = articleRepo.findById(id);
+        model.addAttribute("article", article.get());
+        return "article";
+    }
+
     @GetMapping("/students")
     public String students() {
         return "students";
