bc1824c6dce95de7fbce52c423a557aa087aa2ae
Evil0genius <rybin.evgeney14@gmail.com>
2024-03-05 19:51:16 +0400

    Working login and reg

--- src/main/java/com/example/demo/controllers/UserController.java
+++ src/main/java/com/example/demo/controllers/UserController.java
@@ -2,7 +2,9 @@ package com.example.demo.controllers;
 
 import com.example.demo.model.User;
 import com.example.demo.repo.UserRepo;
+import jakarta.websocket.server.PathParam;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,49 +19,51 @@ public class UserController {
     @Autowired
     UserRepo userRepo;
     @GetMapping("/reg")
-    public String reg(@PathVariable(value = "error")String error, Model model){
+    public String reg(@PathParam(value = "error") String error, Model model){
+        System.out.println(error);
         boolean err = false;
         if(error != null) err = true;
         model.addAttribute("error", err);
         return "reg";
-
-
     }
     @PostMapping("/regUser")
-    public String regUser(@RequestParam String name,
-                      @RequestParam String surname,
-                      @RequestParam String email,
-                      @RequestParam String pass
-                      ){
+    public String regUser(
+            @RequestParam String name,
+            @RequestParam String lastname,
+            @RequestParam String email,
+            @RequestParam String pass){
         Optional<User> oUser = userRepo.findByEmail(email);
         if(oUser.isEmpty()){
-            User user = new User(name, surname, email, pass );
+            User user = new User(name, lastname, email, pass);
             userRepo.save(user);
-            System.out.println("Пльзоватнъель "+name+"существует");
+            System.out.println("Пользователь "+name+" зарегистрирован");
             return "redirect:/login";
-        }else System.out.println("Не найден");
-
+        }else{
+            System.out.println("Такой пользователь уже существует");
+        }
         return "redirect:/reg?error=exist";
     }
     @GetMapping("/login")
-    public String login(@PathVariable(value = "error")String error, Model model){
+    public String login(@PathParam(value = "error") String error, Model model){
         boolean err = false;
         if(error != null){
-            err= true;
+            err = true;
         }
         model.addAttribute("error", err);
         return "login";
     }
     @PostMapping("/login")
-    public String login(@RequestParam String email, @RequestParam String pass){
+    public String login(
+            @RequestParam String email,
+            @RequestParam String pass
+    ){
         Optional<User> oUser = userRepo.findByEmailAndPass(email, pass);
         if(oUser.isEmpty()){
+            System.out.println("Введён неправильный логин или пароль!");
             return "redirect:/login?error=notFound";
-
-        }else {
+        }else{
+            System.out.println("Пользователь "+email+" авторизован");
             return "redirect:/";
         }
-
     }
-
 }
